2819e3f2b0959586183e9d83e3192a72ec2e7f24
mauricedeschamps <mementomorikatsu@gmail.com>
2025-06-20 18:26:53 +0900

    sw.js の更新

--- sw.js
+++ sw.js
@@ -1,10 +1,7 @@
 const CACHE_NAME = 'kansai-travel-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/styles.css',
-  '/app.js',
-  'https://fonts.googleapis.com/css?family=Segoe+UI'
+  './',
+  './index.html'
 ];
 
 self.addEventListener('install', event => {
@@ -17,7 +14,6 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => response || fetch(event.request))
-  );
 });
 
 self.addEventListener('activate', event => {
